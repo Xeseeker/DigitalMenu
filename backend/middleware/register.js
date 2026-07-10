8924a3b77f89cc458073b64b87ee0c44af44269d
xeseeker <lealemtesfazedo@gmail.com>
2026-07-10 10:16:54 +0300

    add login and registration for admin

---
+++ backend/middleware/register.js
@@ -0,0 +1,34 @@
+import express from "express";
+import db from "../config/mysql.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  const { full_name, email, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await db.query(
+      `
+      INSERT INTO admins (full_name, email, password)
+      VALUES (?, ?, ?)
+      `,
+      [full_name, email, hashedPassword],
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "Admin created successfully",
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+});
